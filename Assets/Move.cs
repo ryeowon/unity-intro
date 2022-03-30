dce8bf168efedc683be5cb0e52e42b61e45e7d74
ryeowon <ryeowon13@gmail.com>
2022-03-30 17:36:21 +0900

    add falling of object outside the area

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    Vector3 target = new Vector3(5, 0.5f, 0);
     void Start() {
-        Vector3 vec = new Vector3(0, 0, 0);
-        transform.Translate(vec);
+        //Vector3 vec = new Vector3(0, 0, 0);
+        //transform.Translate(vec);
     }
     void Update()
     {
+        /*
         if (Input.GetKeyDown(KeyCode.Return))
             Debug.Log("아이템을 구입하였습니다.");
 
@@ -27,7 +29,25 @@ public class Move : MonoBehaviour
 
         if (Input.GetButtonUp("Jump")) //Edit - project setting - input manager 에서 확인 가능
             Debug.Log("점프!!!");
+        */
 
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal")*0.02f, 0, Input.GetAxis("Vertical")*0.02f));
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal")*Time.deltaTime*3, 0, Input.GetAxis("Vertical")*Time.deltaTime*3));
+
+        if (transform.position.x > 5 || transform.position.x < -5 || 
+            transform.position.z > 5 || transform.position.z < -5 || transform.position.y < 0.5f)
+            transform.Translate(new Vector3(0, -0.1f, 0));
+
+        if (transform.position.y < -10)
+            transform.position = new Vector3(0, 0.5f, 0);
+
+        //transform.position = Vector3.MoveTowards(transform.position, target, 0.1f);
+
+        //Vector3 velo = Vector3.zero;
+
+        //transform.position = Vector3.SmoothDamp(transform.position, target, ref velo, 0.5f);
+
+        //transform.position = Vector3.Lerp(transform.position, target, 0.05f);
+
+        //transform.position = Vector3.Slerp(transform.position, target, 0.05f);
     }
 }
